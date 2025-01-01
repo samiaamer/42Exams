69220b4fc4382cd5008209fbf36a2fb92508eefb
samia <abutalebsamia@gmai.com>
2025-01-02 01:07:23 +0300

    .

---
+++ level_04/sort_list/sort_list.c
@@ -0,0 +1,24 @@
+#include "list.h"
+
+t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
+{
+    t_list *current;
+    int tmp;
+    
+    if (!lst)
+        return (NULL);
+    current = lst;
+    while (current->next)
+    {
+        if (cmp(current->data, current->next->data) == 0)
+        {
+            tmp = current->data;
+            current->data = current->next->data;
+            current->next->data = tmp;
+            current = lst;
+        }
+        else
+            current = current->next;
+    }
+    return (lst);
+}

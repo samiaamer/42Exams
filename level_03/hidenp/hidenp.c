74d5b1313aefe6ba01e421902268ef2bdb3b41cb
samia abutaleb <abutalebsamia@gmail.com>
2025-01-29 19:19:19 +0300

    .

--- level_03/hidenp/hidenp.c
+++ level_03/hidenp/hidenp.c
@@ -1,22 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hidenp.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabutale <sabutale@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/29 18:20:15 by sabutale          #+#    #+#             */
+/*   Updated: 2025/01/29 19:17:57 by sabutale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 int main(int argc, char **argv)
 {
-    int i1;
-    int i2;
-
-    i1 = 0;
-    i2 = 0;
-    while (argv[2][i2])
-    {
-        if (argv[1][i1] == argv[2][i2])
-            i1++;
-        i2++;
-    }
-     if (argv[1][i1] == '\0')
-            write (1, "1", 1);
-        else 
-            write (1, "0", 1);
-    write (1, "\n", 1);
-    return (0);
+	int len = 0;
+	int i = 0;
+	int j = 0;
+	if (argc == 3)
+	{
+		while (argv[1][len])
+			len++;
+		while (argv[1][i] && argv[2][j])
+		{
+			if (argv[1][i] == argv[2][j])
+			{
+				i++;
+			}
+			j++;
+		}
+		if (i == len)
+			write (1, "1", 1);
+		else
+			write (1, "0", 1);
+	}
+	write (1, "\n", 1);
+	return (0);
 }

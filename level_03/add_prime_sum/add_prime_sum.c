c7d8122b38f571cc75ffd90c89c1e0cda80d5a70
SAMIA ABUTALEB <abutalebsamia@gmail.com>
2025-01-31 23:08:08 +0300

    .

--- level_03/add_prime_sum/add_prime_sum.c
+++ level_03/add_prime_sum/add_prime_sum.c
@@ -1,58 +1,58 @@
+#include <unistd.h>
 
-void	ft_putchar(char c)
+void	ft_putnbr (int n)
 {
-	write(1, &c, 1);
-}
+	char	c;
 
-void	ft_putnbr(int n)
-{
-	if (n > 9)
+	if (n >= 10)
 		ft_putnbr(n / 10);
-	ft_putchar(n % 10 + '0');
+	c = '0' + (n % 10);
+	write (1, &c, 1);
 }
 
-int		ft_atoi(char *s)
+int	is_prime (int n)
 {
-	int		r;
-
-	r = 0;
-	while (*s >= '0' && *s <= '9')
-		r = r * 10 + *s++ - '0';
-	return (r);
-}
-
-int    is_prime(int n)
-{
-    int i;
-
-    i = 2;
-    while (i < n)
-        if (!(n % i++))
-            return (0);
-        return (1);
-}
-
-void    add_prime_sum(int n)
-{
-    int i;
-    int count;
-
-    count = 0;
-    if (n == 1)
-        count = 1;
-    i = 1;
-    while (++i <= n)
-        if (is_prime(i))
-            count += i;
-    ft_putnbr(count);
-}
-
-int		main(int ac, char **av)
-{
-	if (ac == 2 && ft_atoi(av[1]) > 0)
-		add_prime_sum(ft_atoi(av[1]));
-	else
-		write(1, "0", 1);
-	write(1, "\n", 1);
+	int	i = 3;
+	if (n <= 1)
+		return (0);
+	if (n == 2)
+		return (1);
+	if (n % 2 == 0)
+		return (0);
+	while (i * i <= n)
+	{
+		if (n % i == 0)
+			return (0);
+		i += 2;
+	}
 	return (1);
+}
+
+int	ft_atoi (char *s)
+{
+	int	n = 0;
+	while (*s >= '0' && *s <= '9')
+		n = n * 10 + *s++ - '0';
+	return (n);
+}
+
+int	main (int argc, char **argv)
+{
+	if (argc == 2 && ft_atoi(argv[1]))
+	{
+		int	n = ft_atoi(atgv[1]);
+		int	i = 2;
+		int	sum = 0;
+		while (i < = n)
+		{
+			if (is_prime(i))
+				sum += i;
+			i++;
+		}
+		ft_putnbr(sum);
+		write (1, "\n", 1);
+		return (0);
+	}
+	write (1, "0\n", 2);
+	return (0);
 }

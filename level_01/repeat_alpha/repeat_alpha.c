9933e906af13535e505466131b60b1671cee1e26
SAMIA ABUTALEB <abutalebsamia@gmail.com>
2025-02-01 14:52:51 +0300

    .

--- level_01/repeat_alpha/repeat_alpha.c
+++ level_01/repeat_alpha/repeat_alpha.c
@@ -1,33 +1,21 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int    main(int argc, char **argv)
 {
-    int     i;
-    int     repeat;
-
-    repeat = 0;
-    i = 0;
     if (argc == 2)
     {
+        int    i = 0;
+        int    repeat = 0;
         while (argv[1][i])
         {
-            if ((argv[1][i] >= 'a' && argv[1][i] <='z') || ( argv[1][i] >= 'A' && argv[1][i] <= 'Z'))
-            {
-                if (argv[1][i] >= 'a' && argv[1][i] <='z')
-                {
-                    repeat = argv[1][i] - 'a' + 1;
-                }
-                else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-                {
-                    repeat = argv[1][i] - 'A' + 1;
-                }
-                while (repeat > 0)
-                {
-                    write (1, &argv[1][i], 1);
-                    repeat--;
-                }
-            }
+            repeat = 0;
+            if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+                repeat = argv[1][i] - 'a' + 1;
+            else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+                repeat = argv[1][i] - 'A' + 1;
             else
+                write (1, &argv[1][i], 1);
+            while (repeat--)
                 write (1, &argv[1][i], 1);
             i++;
         }

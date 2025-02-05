7495b65b9f1347c8f20886d773fd5acdc4fcf8e9
SAMIA ABUTALEB <abutalebsamia@gmail.com>
2025-02-05 16:58:21 +0300

    Update first_word.c

--- level_01/first_word/first_word.c
+++ level_01/first_word/first_word.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int     main(int argc, char **argv)
 {
-    int i;
-
-    i = 0;
+    int i = 0;
     if (argc == 2)
     {
-        while (argv[1][i])
+        while (argv[1][i] && argv[1][i] == ' ' || argv[1][i] == '\t')
+            i++;
+        while (argv[1][i] && argv[1][i] != ' ' || argv[1][i] != '\t')
         {
             write (1, &argv[1][i], 1);
             i++;

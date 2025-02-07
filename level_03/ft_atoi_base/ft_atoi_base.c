a5c3fc12be16063366efd1768f405f899c1b36ca
SAMIA ABUTALEB <abutalebsamia@gmail.com>
2025-02-07 20:30:57 +0300

    Update ft_atoi_base.c

--- level_03/ft_atoi_base/ft_atoi_base.c
+++ level_03/ft_atoi_base/ft_atoi_base.c
@@ -3,21 +3,29 @@ int     ft_atoi_base(const char *str, int str_base)
     int i = 0;
     int n = 1;
     int res = 0;
-    if(str[i] == '-')
-    {
-        n = -1;
-        i++;
-    }
+    if (str_base < 2 || str_base > 16)
+        return (0);
+    if (str[i] == '-')
+        {
+            n = -1;
+            
+            i++;
+        }
     while (str[i])
-    {
-        res *= str_base;
-        if (str[i] >= '0' && str[i] <= '9')
-            res += str[i] - 48;
-        if (str[i] >= 'A' && str[i] <= 'Z')
-            res += str[i] - 55;
-        if (str[i] >= 'a' && str[i] <= 'z')
-            res += str[i] - 87;
-        i++;
-    }
+        {
+            int digit = 0;
+            if (str[i] >= '0' && str[i] <= '9')
+                digit += str[i] - 48;
+            else if (str[i] >= 'A' && str[i] <= 'F')
+                digit += str[i] - 'A' + 10;                                                                                    
+            else if (str[i] >= 'a' && str[i] <= 'f')                                                                                        
+                digit += str[i] - 'a' + 10;                                                                                    
+            else                                                                                                                            
+                break;                                                                                                          
+            if (digit >= str_base)                                                                                                         
+                break;                                                                                                         
+            res = res * str_base + digit;                                                                                          
+            i++;                                                                                                            
+        }                                                                                                                       
     return (res * n);
 }

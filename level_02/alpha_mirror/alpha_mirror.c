2872af8b42142a16e08272f624d0161a06d06922
SAMIA ABUTALEB <abutalebsamia@gmail.com>
2025-02-07 18:57:21 +0300

    Update alpha_mirror.c

--- level_02/alpha_mirror/alpha_mirror.c
+++ level_02/alpha_mirror/alpha_mirror.c
@@ -11,8 +11,10 @@ int     main(int argc, char **argv)
         {
             if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
                 mirror = 'z' - (argv[1][i] - 'a');
+                //argv[1][i] = ('a' + 'z') - argv[1][i];
             else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
                 mirror = 'Z' - (argv[1][i] - 'A');
+                //argv[1][i] = ('A' + 'Z') - argv[1][i];
             else   
                 mirror = argv[1][i];
             write (1, &mirror, 1);

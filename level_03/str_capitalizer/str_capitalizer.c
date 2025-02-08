d3a2f3b863430ff968ae8f4f6f8680ddfcd8c888
SAMIA ABUTALEB <abutalebsamia@gmail.com>
2025-02-08 22:05:40 +0300

    Update str_capitalizer.c

---
+++ level_03/str_capitalizer/str_capitalizer.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+void  str_cap(char *str)
+{
+  int i = 0;
+  if (str[i] >= 'a' && argv[1][i] <= 'z')
+    str[i] -= 32;
+  write (1, &str[i], 1);
+  i++;
+  while (str[i])
+    {
+      if (str[i] >= 'A' && str[i] <= 'Z')
+        str[i] += 32;
+      if ((str[i] >= 'a' && srt[i] <= 'z') && (srt[i - 1] == ' '  || str[i - 1] == '\t'))
+        str[i] -= 32;
+      write (1, &str[i], 1);
+      i++;
+    }
+}
+
+int  main(int argc, char **argv)
+{
+  if (argc > 1)
+  {
+    int i = 0;
+    while (i < argc)
+      {
+        str_cap(argv[i]);
+        write(1, "\n", 1);
+        i++;
+      }
+  }
+  write (1, "\n", 1);
+  return (0);
+}

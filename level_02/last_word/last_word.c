496e0575e9c7f2b8e871d6c8a45cb489a073ce50
SAMIA ABUTALEB <abutalebsamia@gmail.com>
2025-02-08 21:45:33 +0300

    Update last_word.c

--- level_02/last_word/last_word.c
+++ level_02/last_word/last_word.c
@@ -2,5 +2,20 @@
 
 int main(int argc, char **argv)
 {
-    
+    if (argc == 2)
+    {
+        int i = 0;
+        while (argv[1][i])
+            i++;
+        i--;
+        while (i >= 0 && (argv[1][i] == ' ' || argv[1][i] == '\t')
+            i--;
+        while (i >= 0 && (argv[1][i] != ' ' && argv[1][i] != '\t')
+            i--;
+        i++;
+        while (argv[1][i] && (argv[1][i] != ' ' && argv[1][i] != '\t'))
+            write (1, &argv[1][i++], 1);
+    }
+    write (1, "\n", 1);
+    return (0);
 }

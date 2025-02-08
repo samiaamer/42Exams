7b0a454f2f0b0f44c6abdebdd04f99c1209fbd8c
SAMIA ABUTALEB <abutalebsamia@gmail.com>
2025-02-08 22:38:38 +0300

    Update rev_wstr.c

---
+++ level_04/rev_wstr/rev_wstr.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+
+int  main(int argc, char **argv)
+{
+  if (argc == 2)
+  {
+    int s;
+    int e;
+    int i = 0;
+    while (argv[1][i])
+      i++;
+    while (i >= 0)
+      {
+        while (argv[1][i] == '\0' || argv[1][i] == ' ' || argv[1][i] == '\t')
+          i--;
+        end = i;
+        while (argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
+          i--;
+        start = i + 1;
+        int flag = start;
+        while (start <= end)
+          {
+            write (1, &argv[1][start], 1);
+            start++;
+          }
+        if (flag)
+          write (1, " ", 1);
+      }
+  }
+  write (1, "\n", 1);
+  return (0);
+}

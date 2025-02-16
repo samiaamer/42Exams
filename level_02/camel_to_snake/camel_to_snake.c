60aaf90888a836fece5dfe0fe241beb697207116
SAMIA ABUTALEB <abutalebsamia@gmail.com>
2025-02-16 13:07:10 +0300

    Update camel_to_snake.c

--- level_02/camel_to_snake/camel_to_snake.c
+++ level_02/camel_to_snake/camel_to_snake.c
@@ -1,36 +1,20 @@
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
-    int i;
-    int j;
-    char *result;
-
-    j = 0;
-    i = 0;
-    if (argc == 2)
-    {
-        while (argv[1][i])
-            i++;
-        result = malloc((i * 2) + 1);
-        i = 0;
-        while (argv[1][i])
-        {
-            if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-            {
-                if (i != 0)
-                    result[j++] = '_';
-                result[j++] = argv[1][i] + 32;
-            }
-            else
-                result[j++] = argv[1][i];
-            i++;
-        }
-        result[j] = '\0';
-        write (1, result, j);
-        free(result);
-    }
-    write (1, "\n", 1);
-    return (0);
+	if (argc == 2)
+	{
+		for (int i = 0; argv[1][i]; i += 1)
+		{
+			if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+			{
+				write(1, "_", 1);
+				argv[1][i] += 32;
+			}
+			write(1, &argv[1][i], 1);
+		}
+	}
+	write(1, "\n", 1);
+	return 0;
 }
